e829808ff0d543c4bb3f2879efc95fe2f9eebbfd
Chris Child <thechild@gmail.com>
2012-11-21 17:27:18 -0500

    Cleaned up code, added calls for user access control

--- Messages/views.py
+++ Messages/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import get_object_or_404, render_to_response, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.views.generic import ListView
 from Messages.models import User, Group, Thread, Message
 from django import forms
+
+class UserForm(forms.ModelForm):
+	class Meta:
+		model = User
+
+class ThreadForm(forms.Form):
+	def __init__(self, user, *args, **kwargs):
+		super(ThreadForm, self).__init__(*args, **kwargs)
+		users = User.objects.all().exclude(pk=user.pk)
+		
+		self.fields['members'] = forms.MultipleChoiceField(
+			widget=forms.CheckboxSelectMultiple,
+			choices=[ (u.pk, u.name()) for u in users])
+
+	members = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
+	subject = forms.CharField(max_length=100)
+	message_text = forms.CharField(widget=forms.Textarea)
+
+#############################
+#### View Only Functions ####
+#############################
 
 def user_messages(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
@@ -13,25 +34,35 @@ def user_messages(request, user_id):
 def view_thread_as_user(request, user_id, thread_id):
 	user = get_object_or_404(User, pk=user_id)
 	thread = get_object_or_404(Thread, pk=thread_id)
+	if not thread.user_has_access(user):
+		return HttpResponseForbidden('You do not have access to that thread') # prob change this to a 404 at some point
 	return render_to_response('thread_detail.html', {'user': user, 'thread': thread},
 		context_instance=RequestContext(request))
 
 def view_group_as_user(request, user_id, group_id):
 	user = get_object_or_404(User, pk=user_id)
 	group = get_object_or_404(Group, pk=group_id)
-	return render_to_response('group_detail.html', {'user': user, 'group': group},
+
+	if not group.user_has_access(user):
+		return HttpResponseForbidden('You do not have access to that group') # prob change this to a 404 at some point
+
+	# related groups
+	r_groups = Group.objects.filter(members = user).exclude(pk = group.pk)
+	for m in group.members.exclude(pk = user.pk):
+		r_groups = r_groups.filter(members = m)
+
+	return render_to_response('group_detail.html', {'user': user, 'group': group, 'related_groups': r_groups},
 		context_instance=RequestContext(request))
+
+##########################
+#### Creation Methods ####
+##########################
 
 def new_message(request, user_id, thread_id):
 	user = get_object_or_404(User, pk=user_id)
 	thread = get_object_or_404(Thread, pk=thread_id)
 	message_text = request.POST['message_text']
-
-	message = Message()
-	message.sender = user
-	message.thread = thread
-	message.text = message_text
-	message.html = message_text
+	message = Message(sender = user, thread = thread, text = message_text, html = message_text)
 	message.save()
 	return HttpResponseRedirect(reverse('thread_detail', args=(user_id, thread_id,)))
 
@@ -52,27 +83,34 @@ def new_member(request, user_id):
 			{'user': user, 'form': UserForm() },
 			context_instance=RequestContext(request))
 
-class UserForm(forms.ModelForm):
-	class Meta:
-		model = User
+# Return an existing or new group with the given members (including creator)
+def group_with_members(creator, members):
+	group = Group.objects.filter(members=creator)
+	for m in members:
+		group = group.filter(members=m)
+		if group.count() == 0:
+			break
 
-class ThreadForm(forms.Form):
-	def __init__(self, user, *args, **kwargs):
-		super(ThreadForm, self).__init__(*args, **kwargs)
-		users = User.objects.all().exclude(pk=user.pk)
-		
-		self.fields['members'] = forms.MultipleChoiceField(
-			widget=forms.CheckboxSelectMultiple,
-			choices=[ (u.pk, u.name()) for u in users])
+	if group.count() == 0:
+		# couldn't find this exact group, so create one
+		group = Group()
+		group.creator = creator
+		group.save()
+		group.members.add(creator)
+		for m in members:
+			group.members.add(m)
+		group.save()
+	elif group.count() == 1:
+		group = group[0]
+	else:
+		print "we somehow got multiple groups with these members: %s" % (creator, members)
+		group = group[0]
 
-	members = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
-	subject = forms.CharField(max_length=100)
-	message_text = forms.CharField(widget=forms.Textarea)
+	return group
 
-# this logic really shouldn't live here!
 def new_thread(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
-	users = User.objects.all().exclude(pk=user_id)
+	users = User.objects.all().exclude(pk=user_id) # all users except for the logged in one.  Obviously this needs to get smarter!
 	if request.method == 'POST':
 		print 'we posted'
 		form = ThreadForm(user, request.POST)
@@ -81,43 +119,17 @@ def new_thread(request, user_id):
 			members = [User.objects.get(pk=m) for m in form.cleaned_data['members']]
 			print members
 
-			group = Group.objects.filter(members=user)
-			print 'group: %s' % group
-			for m in members:
-				group = group.filter(members=m)
-				if group == []:
-					print 'breaking'
-					break
-
-			if group.count() == 0:
-				group = Group()
-				group.creator = user
-				group.save()
-				group.members.add(user)
-				for m in members:
-					group.members.add(m)
-				print group.members.count()
-				group.save()
-			else:
-				group = group[0]
-
-			print 'group: %s' % group
-
-			# now we have the group, so create the thread:
-
-			thread = Thread()
-			thread.subject = form.cleaned_data['subject']
-			thread.creator = user
-			thread.group = group
+			group = group_with_members(user, members)
+			thread = Thread(subject = form.cleaned_data['subject'],
+							creator = user,
+							group = group)
 			thread.save()
 
 			# finally add the message
-
-			message = Message()
-			message.sender = user
-			message.text = form.cleaned_data['message_text']
-			message.html = message.text
-			message.thread = thread
+			message = Message(sender = user,
+							  text = form.cleaned_data['message_text'],
+							  html = form.cleaned_data['message_text'],
+							  thread = thread)
 			message.save()
 
 			return HttpResponseRedirect(reverse('thread_detail', args=(user.pk, thread.pk,)))
@@ -127,4 +139,3 @@ def new_thread(request, user_id):
 		form = ThreadForm(user)
 
 	return render(request, 'new_thread.html', {'user': user, 'form': form})
-	#return render_to_response('new_thread.html', {'user': user, 'users': users}, context_instance=RequestContext(request))
